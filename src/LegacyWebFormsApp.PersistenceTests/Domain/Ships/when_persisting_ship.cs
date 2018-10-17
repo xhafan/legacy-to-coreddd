130a5a5d5ff21c101452b7889cf87970737edb78
xhafan <xhafan@gmail.com>
2018-10-17 23:19:14 +0200

    added persistence tests project

---
+++ src/LegacyWebFormsApp.PersistenceTests/Domain/Ships/when_persisting_ship.cs
@@ -0,0 +1,21 @@
+﻿using CoreDdd.Nhibernate.TestHelpers;
+using NUnit.Framework;
+
+namespace LegacyWebFormsApp.PersistenceTests.Domain.Ships
+{
+    [TestFixture]
+    public class when_persisting_ship : BasePersistenceTest
+    {
+        [SetUp]
+        public void Context()
+        {
+            
+        }
+
+        [Test]
+        public void BEHAVIOUR()
+        {
+            
+        }
+    }
+}
