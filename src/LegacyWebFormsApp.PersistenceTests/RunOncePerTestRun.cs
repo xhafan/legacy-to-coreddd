091e2496ffa265317d7c91c40edc1a3bf0264c47
xhafan <xhafan@gmail.com>
2018-10-18 01:33:02 +0200

    added Ship persistence test

---
+++ src/LegacyWebFormsApp.PersistenceTests/RunOncePerTestRun.cs
@@ -0,0 +1,31 @@
+﻿using System.Data.SqlClient;
+using CoreDdd.Nhibernate.DatabaseSchemaGenerators;
+using NUnit.Framework;
+
+namespace LegacyWebFormsApp.PersistenceTests
+{
+    [SetUpFixture]
+    public class RunOncePerTestRun
+    {
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            _CreateDatabase();
+        }
+
+        private void _CreateDatabase()
+        {
+            using (var nhibernateConfigurator = new LegacyWebFormsAppNhibernateConfigurator())
+            {
+                var configuration = nhibernateConfigurator.GetConfiguration();
+                var connectionString = configuration.Properties["connection.connection_string"];
+
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    new DatabaseSchemaCreator().CreateDatabaseSchema(nhibernateConfigurator, connection);
+                }
+            }
+        }
+    }
+}
