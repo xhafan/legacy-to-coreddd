d7c77b4e776a035039d26ce543080ea6e68691ec
xhafan <xhafan@gmail.com>
2018-10-18 08:55:57 +0200

    Added nhibernate profiler

--- src/LegacyWebFormsApp/LegacyWebFormsAppNhibernateConfigurator .cs
+++ src/LegacyWebFormsApp/LegacyWebFormsAppNhibernateConfigurator .cs
@@ -1,14 +1,35 @@
 ﻿using System.Reflection;
 using CoreDdd.Nhibernate.Configurations;
 using LegacyWebFormsApp.Domain;
+#if DEBUG
+using HibernatingRhinos.Profiler.Appender.NHibernate;
+#endif
 
 namespace LegacyWebFormsApp
 {
     public class LegacyWebFormsAppNhibernateConfigurator : NhibernateConfigurator
     {
+#if DEBUG
+        public LegacyWebFormsAppNhibernateConfigurator()
+        {
+            NHibernateProfiler.Initialize();
+        }
+#endif
+
         protected override Assembly[] GetAssembliesToMap()
         {
             return new[] { typeof(Ship).Assembly };
         }
+
+#if DEBUG
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                NHibernateProfiler.Shutdown();
+            }
+            base.Dispose(disposing);
+        }
+#endif
     }
 }
