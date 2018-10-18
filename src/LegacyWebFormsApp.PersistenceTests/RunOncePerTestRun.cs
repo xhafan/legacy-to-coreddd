be478b57db5d586d4e256bc36f8de2110e31f5bd
xhafan <xhafan@gmail.com>
2018-10-18 08:41:50 +0200

    converted persistence test project into new visual studio 2017 csproj format

--- src/LegacyWebFormsApp.PersistenceTests/RunOncePerTestRun.cs
+++ src/LegacyWebFormsApp.PersistenceTests/RunOncePerTestRun.cs
@@ -1,5 +1,9 @@
-﻿using System.Data.SqlClient;
+﻿using System;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Data.SQLite;
 using CoreDdd.Nhibernate.DatabaseSchemaGenerators;
+using Npgsql;
 using NUnit.Framework;
 
 namespace LegacyWebFormsApp.PersistenceTests
@@ -18,13 +22,29 @@ namespace LegacyWebFormsApp.PersistenceTests
             using (var nhibernateConfigurator = new LegacyWebFormsAppNhibernateConfigurator())
             {
                 var configuration = nhibernateConfigurator.GetConfiguration();
+                var connectionDriverClass = configuration.Properties["connection.driver_class"];
                 var connectionString = configuration.Properties["connection.connection_string"];
 
-                using (var connection = new SqlConnection(connectionString))
+                using (var connection = _CreateDbConnection(connectionDriverClass, connectionString))
                 {
                     connection.Open();
                     new DatabaseSchemaCreator().CreateDatabaseSchema(nhibernateConfigurator, connection);
                 }
+            }
+        }
+
+        private DbConnection _CreateDbConnection(string connectionDriverClass, string connectionString)
+        {
+            switch (connectionDriverClass)
+            {
+                case string x when x.Contains("SQLite"):
+                    return new SQLiteConnection(connectionString);
+                case string x when x.Contains("SqlClient"):
+                    return new SqlConnection(connectionString);
+                case string x when x.Contains("NpgsqlDriver"):
+                    return new NpgsqlConnection(connectionString);
+                default:
+                    throw new Exception("Unsupported NHibernate connection.driver_class");
             }
         }
     }
